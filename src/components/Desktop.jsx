84775fdfb0f5a11a3292af7c15b9d8d6d7538eed
Abdullah Younas <abdullahyounas24082005@gmail.com>
2026-01-10 01:26:47 +0500

    Empty Functions for the folders

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react'
 import './Desktop.css'
+import myResume from '../assets/Malik Muhammad Abdullah Younas Resume.pdf'
 
 function Desktop() {
+
+    function OpenWebFolder(){
+        console.log("Opening web folder!");
+    }
+
+    function OpenGameFolder(){
+        console.log("Opening game folder!");
+    }
+
+
+
   return (
     <>
         <div className="Desktop">
-            <div className='Icon' onClick={() => console.log("it's mee web projects")}>
+            <div className='Icon' onClick={OpenWebFolder}>
                 📁
             </div>
             <div className='Icon' onClick={() => console.log("it's mee About.txt")}>
